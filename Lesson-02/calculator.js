a1f25d599ef1889b7f949dd780a4c49b47860c4b
Dane Seaberg <>
2021-01-27 22:38:28 -0800

    Add a basic calculator that prints out results from each operation.

---
+++ Lesson-02/calculator.js
@@ -0,0 +1,19 @@
+let valueA = 7;
+let valueB = 2;
+let valueC = 12;
+let valueD = -3
+
+let resultAddition = valueA + valueD;
+console.log('A + D: ' + resultAddition);
+
+let resultSubtraction = valueC - valueB;
+console.log('C - B: ' + resultSubtraction);
+
+let resultMultiplication = valueA * valueB;
+console.log('A * B: ' + resultMultiplication);
+
+let resultDivision = valueC / valueD;
+console.log('C / D: ' + resultDivision);
+
+let resultModulus = valueC % valueA;
+console.log('C % A: ' + resultModulus);
