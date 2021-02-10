06b0dcd54d216b497fc400bb6269589aa565f11a
Dane Seaberg <>
2021-02-09 21:27:58 -0800

    Implement lesson #4 - exercise #1

---
+++ Lesson-04/script.js
@@ -0,0 +1,8 @@
+function numberAdder() {
+  let firstNumber = prompt('Enter a number');
+  let secondNumber = prompt('Enter another');
+
+  let result = Number(firstNumber) * Number(secondNumber);
+
+  alert(result);
+}
